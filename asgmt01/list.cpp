475bbbdec0a07c69725e39d2f26fbc7d6f98e0a0
201504 40277 paulE. Hubbard <paul.hubbard@syccuxas01.pcc.edu>
2016-10-09 17:27:22 -0700

    asgmt.4

--- asgmt01/list.cpp
+++ asgmt01/list.cpp
@@ -5,45 +5,51 @@ using namespace std;
 
 const char	List::YOUR_NAME[] = "Paul Hubbard";
 
-List::List() 
+List::List() :
+	headByName{nullptr},
+	headByIntensity{nullptr}
 {
 	// your code here, or in this constructor's initialization list
-	headByName = nullptr;
-	headByIntensity = nullptr;
 }
 
 List::~List() 
 {
 	// your code here
 	Node * nodeName{headByName};
-	Node * nodeIntensity{headByIntensity};
-	Node * nextByInt;
-	Node * nextByName;
+	Node * nextName;
 
-	while(nodeName && nodeIntensity){
-		nextByInt = nodeIntensity->nextByIntensity;
-		nextByName = nodeName->nextByName;
+	while(nodeName){
+		nextName = nodeName->nextByName;
 		delete nodeName;
-		delete nodeIntensity;
-		nodeIntensity = nextByInt;
-		nodeName = nextByName;
+		nodeName = nextName;
 	}
 	
 }
 
-/*
-You will need to uncomment this constructor and write the code for it.
 
-List::Node::Node(const Voter& voter)
+//You will need to uncomment this constructor and write the code for it.
+
+List::Node::Node(const Voter& voter) :
 	// your initialization list here
+	item{voter.getName(), voter.getParty(), voter.getIntensity()},
+	nextByName{nullptr},
+	nextByIntensity{nullptr}
 {
 	// your code here
 }
-*/
+
 
 void List::displayByName(ostream& out) const
 {
 	// your code here
+	Node * node{headByName};
+
+	Voter::displayColumnHeadings(out);
+
+	while(node){
+		out << &node->item << endl;
+		node = node->nextByName;
+	}
 }
 
 void List::displayByIntensity(ostream& out) const
@@ -54,6 +60,18 @@ void List::displayByIntensity(ostream& out) const
 void List::insert(const Voter& voter)
 {
 	// your code here
+	Node * node = new Node(voter);
+//	Node * nodeByInten{headByIntensity};
+//	Node * nextName;
+//	Node * nextByIntensity;
+
+	if(headByName == nullptr){
+		 headByName = node;
+		 headByIntensity = node;
+	}
+
+		
+
 }
 
 Voter* const List::find(const char * const name) const
--- asgmt01/voter.cpp
+++ asgmt01/voter.cpp
@@ -1,31 +1,57 @@
 #include <iostream>
 #include <string.h>
+#include <iomanip>
 #include "voter.h"
 
 using namespace std;
 
-Voter::Voter(const char * const name, const Party party, const int intensity) :
-name{nullptr},
-party{},
-intensity{0}
+Voter::Voter(const char * const nme, const Party party, const int intensity) :
+party{party},
+intensity{intensity}
 {
 	// your code here, or in this constructor's initialization list
+	name = {new char[strlen(nme) + 1]};
+	strcpy(name, nme);
 }
 
 Voter::~Voter()
 {
 	// your code here
-	delete name;
+	delete [] name;
 }
 
 void Voter::displayColumnHeadings(ostream& out)
 {
 	// your code here
+	out << left << setw(26) << "name"
+				<< setw(17) << "party"
+				<< setw(9)  << "intensity"
+				<< endl;
+	out << left << setw(26) << "----"
+				<< setw(17) << "-----"
+				<< setw(9)  << "---------"
+				<< endl;
+
+
 }
 
 ostream& operator<<(ostream& out, Voter* voter)
 {
 	// your code here
+	out << left << setw(26) << 	voter->name
+				<< setw(17) << 	voter->party
+	    << right<< setw(9)  << 	voter->intensity
+				<< endl;
 
+	return out;
+}
+
+ostream& operator<<(std::ostream& out, Party party){
+	switch(party){
+		case Party::Republican:   out << "Republican"; break;
+		case Party::Democrat:     out << "Democrat";   break;
+		case Party::Libertarian:  out << "Libertarian";break;
+		case Party::Green:		  out << "Green";      break;        
+	}
 	return out;
 }
