4b677a93efe858f551d3b11bf5a1680ee9edc36d
201504 40277 paulE. Hubbard <paul.hubbard@syccuxas01.pcc.edu>
2016-11-04 19:59:31 -0700

    asgmt02.1.9

--- asgmt02/quack.cpp
+++ asgmt02/quack.cpp
@@ -8,7 +8,7 @@ using namespace std;
 const char	Quack::YOUR_NAME[]{"Paul Hubbard"};
 
 // change the value of this variable to true if you're doing the Premium version
-const bool	Quack::PREMIUM_VERSION{false};
+const bool	Quack::PREMIUM_VERSION{true};
 
 Quack::Quack(int capacity, int growBy) :
 	// replace these initializations with the ones you actually need
@@ -50,6 +50,27 @@ bool Quack::pushFront(const char ch)
 			counter++;
 			return true;
 		}
+		if(counter + 1 == capacity){
+			int newCapacity = capacity + growBy;
+			char * newItems = new char[newCapacity];
+			for(int i = 0; i < capacity; i++){
+				newItems[i] = '\0';
+			}
+			
+			for(int i = 0; i < capacity; i++){
+				items[i] = newItems[i];
+			}
+
+			items = newItems;
+			capacity = newCapacity;
+			
+			frontIndex = frontIndex - 1;
+			items[frontIndex] = ch;
+			counter++;
+			
+			delete newItems;
+			return true;
+		}
 
 	}
 
@@ -60,21 +81,40 @@ bool Quack::pushFront(const char ch)
 // (except for when the end of the array "wraps around" to the beginning)
 bool Quack::pushBack(const char ch)
 {	
-	
-	if(items[backIndex]){
-		backIndex = backIndex + 1;
+	if(counter == 0){
 		items[backIndex] = ch;
+		counter += 1;
+		return true;
+	}
+	else{
+		backIndex = (backIndex  == (capacity - 1) ? 0 : backIndex + 1);
+			items[backIndex] = ch;
+			counter += 1;
+			return true;
+	}
 
-		//items[backIndex + 1] = ch;
-		//backIndex = backIndex + 1;
-		counter += 1;
-		return true;
-	}
-	else if(counter == 0){
-		items[backIndex] = ch;
-		counter += 1;
-		return true;
-	}
+		if(counter + 1 == capacity){
+			int newCapacity = capacity + growBy;
+			char * newItems = new char[newCapacity];
+			for(int i = 0; i < capacity; i++){
+				newItems[i] = '\0';
+			}
+			
+			for(int i = 0; i < capacity; i++){
+				items[i] = newItems[i];
+			}
+
+			items = newItems;
+			capacity = newCapacity;
+			
+			backIndex = (backIndex  == (capacity - 1) ? 0 : backIndex + 1);
+			items[backIndex] = ch;
+			counter += 1;
+			
+			delete newItems;
+			return true;
+		}
+
 	return false;
 }
 
@@ -87,12 +127,14 @@ bool Quack::popFront(char& ch)
 		
 		if(frontIndex == capacity - 1){
 			frontIndex = 0;
+			counter = counter - 1;
 			return true;
 		}else{
 			frontIndex = frontIndex + 1;
 			counter = counter - 1;
 			return true;
 		}
+		
 	}
 	return false;
 }
@@ -118,28 +160,69 @@ bool Quack::popBack(char& ch)
 
 void Quack::rotate(int r)
 {
-	if(counter != capacity){
+	char ch;
 		if(r > 0){
-			int index = frontIndex;
+			for(int i = 0; i< r; i++)
+			{	
+				ch = items[frontIndex];
+		
+				if(frontIndex == capacity - 1){
+					frontIndex = 0;
+					counter = counter - 1;
+				}else{
+					frontIndex = frontIndex + 1;
+					counter = counter - 1;
+				}
+				
+				if(counter == 0){
+					items[backIndex] = ch;
+					counter += 1;
+				}
+				else{
+					backIndex = (backIndex  == (capacity - 1) ? 0 : backIndex + 1);
+					items[backIndex] = ch;
+					counter += 1;
+				}
 
-			items[backIndex + 1] = items[frontIndex];
-			for(int i = 0; i < capacity - counter; i++){
-				items[index] = items[index + 1];
-				index = index + 1;
-			}
-			frontIndex = frontIndex + r;
-			backIndex = backIndex + r;
-			if(frontIndex >= capacity){
-				frontIndex = frontIndex - capacity;
 			}
 		}else if(r < 0){
-			
+			for(int i = r; i < 0; i++){
+				
+				ch = items[backIndex];
+
+				if(backIndex == (0)){
+					backIndex = capacity - 1;
+					counter = counter - 1;
+				}else{
+					backIndex = backIndex - 1;
+					counter = counter - 1;
+				}
+		
+				if(counter == 0){
+					items[0] = ch;
+					counter++;
+				}
+				else{
+					if(frontIndex == 0){ 
+						frontIndex = capacity - 1;
+						items[frontIndex] = ch;
+						counter++;
+					}
+					else{
+						frontIndex = frontIndex - 1;
+						items[frontIndex] = ch;
+						counter++;
+					}
+			}
 		}
 	}
 }
 
 void Quack::reverse(void)
 {
+
+		
+	
 }
 
 int	Quack::itemCount(void)
